d7d962891a0775a3432da7e01cb4645103411b04
silvamaarcus <devsilvamarcus@gmail.com>
2024-07-17 15:00:56 -0300

    Section Menu...

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,24 +1,24 @@
 // Logo
-import logo from "../../../public/assets/svg/logo.svg";
+import logo from "/assets/svg/logo.svg";
 
 const Menu = () => {
   return (
     <>
-      <div className="bg-header flex-center">
+      <div className="bg-header flex-center flex-wrap py-1">
         <div className="grid-1 disappear"></div>
 
-        <div className="grid-7">
-          <div className="flex-start-row border gap-3">
+        <div className="grid-7 p-0">
+          <div className="flex-start-row content-mobile gap-3">
             <img src={logo} alt="UFMG" className="logo" />
-            <h6 className="color-white uppercase bold">
+            <h6 className="color-white uppercase bold text-center">
               IX SEMANA DE SAÚDE MENTAL E INCLUSÃO SOCIAL
             </h6>
           </div>
         </div>
 
-        <div className="grid-5 border">
-          <div className="flex-end-row">
-            <input type="text" className=" w-75" placeholder="Faça sua busca"/>
+        <div className="grid-5 p-0">
+          <div className="flex-end-row content-mobile no-break-mobile">
+            <input type="text" className=" w-60" placeholder="Faça sua busca" />
             <div className="btn-search"></div>
           </div>
         </div>
@@ -26,7 +26,31 @@ const Menu = () => {
         <div className="grid-1 disappear"></div>
       </div>
 
-      <div className="bg-menu"></div>
+      <div className="bg-menu flex">
+        <div className="grid-1 disappear"></div>
+
+        <div className="grid-12 p-0">
+          <ul className="menu-links flex-start-row gap-5 hidden">
+            <li>
+              <a href="/" className="uppercase">Option</a>
+            </li>
+            <li>
+              <a href="/" className="uppercase">Option</a>
+            </li>
+            <li>
+              <a href="/" className="uppercase">Option</a>
+            </li>
+            <li>
+              <a href="/" className="uppercase">Option</a>
+            </li>
+            <li>
+              <a href="/" className="uppercase">Option</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="grid-1 disappear"></div>
+      </div>
     </>
   );
 };
